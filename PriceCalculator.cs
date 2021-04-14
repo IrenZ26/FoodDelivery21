4407d060795a077dbf377dfdc285834a8530572d
irenz <iren1256799@gmail.com>
2021-04-14 16:03:48 +0300

    changes made

--- PriceCalculator.cs
+++ PriceCalculator.cs
@@ -15,18 +15,13 @@ namespace FoodDelivery
                                    IEnumerable<decimal> prices,
                                    IEnumerable<string> currencies)
         {
+
             decimal fullPrice = default;
-            List<string> destinationsList = destinations.ToList();
-            List<string> clientsList = clients.ToList();
-            List<string> currenciesList = currencies.ToList();
-            List<int> infantsIdsList = infantsIds.ToList();
-            List<int> childrenIdsList = childrenIds.ToList();
-            List<decimal> pricesList = prices.ToList();
             
-            if (Validation(destinationsList.Count(), clientsList.Count(), pricesList.Count(), currenciesList.Count())) 
+            if (Validator(destinations.Count(), clients.Count(), prices.Count(), currencies.Count())) 
             {
-                pricesList = PriceDiscount(destinationsList, clientsList, pricesList, infantsIdsList, childrenIdsList, currenciesList);
-                foreach (decimal i in pricesList)
+                prices = PriceDiscounter(destinations, prices, infantsIds, childrenIds, currencies);
+                foreach (decimal i in prices)
                 {
                     fullPrice += i;
                 }
@@ -37,7 +32,7 @@ namespace FoodDelivery
             }
             return fullPrice;
         }
-        private bool Validation(int destinationsCount, int clientsCount, int pricesCount, int currenciesCount) {
+        private bool Validator(int destinationsCount, int clientsCount, int pricesCount, int currenciesCount) {
             if ((destinationsCount.Equals(clientsCount))&&(destinationsCount.Equals(pricesCount)) && (destinationsCount.Equals(currenciesCount)))
             {
                 return true;
@@ -47,60 +42,62 @@ namespace FoodDelivery
                 return false;
             }
         }
-        private List<decimal> PriceDiscount(List<string> destinationsList, List<string> clientsList, List<decimal> pricesList, List<int> infantsIdsList, List<int> childrenIdsList, List<string> currenciesList)
+        private IEnumerable<decimal> PriceDiscounter(IEnumerable<string> destinations, IEnumerable<decimal> prices, IEnumerable<int> infantsIds, IEnumerable<int> childrenIds, IEnumerable<string> currencies)
         {
             decimal finalPrice;
             string s,s1;
-            for(int i = 0; i < pricesList.Count; i++)
+            List<decimal> pricesList = prices.ToList();
+            for (int i = 0; i < pricesList.Count; i++)
             {
-                if (currenciesList[i]== "EUR") { pricesList[i] =  СurrencyConverter(pricesList[i]);}
+                if (currencies.ElementAt(i)== "EUR") { pricesList[i] =  СurrencyConverter(pricesList[i]);}
                 decimal discount = 0;
                 decimal discountedPrice = 0;
-                finalPrice = PriceDiscountAdress(pricesList[i], destinationsList[i]);
-                s = GetStreet(destinationsList[i]);
+                finalPrice = PriceDiscountAddress(pricesList[i], destinations.ElementAt(i));
+                s = GetStreet(destinations.ElementAt(i));
                 if (i > 0) 
                 {
-                    s1 = GetStreet(destinationsList[i-1]);
+                    s1 = GetStreet(destinations.ElementAt(i-1));
                     if (s == s1) 
                     {
                         discount += 0.15m;
                     }
                 }
-                if (infantsIdsList.Contains(i)) 
+                if (infantsIds.Contains(i)) 
                 {
                     discount += 0.5m;
                 }
-                if (childrenIdsList.Contains(i))
+                if (childrenIds.Contains(i))
                 {
                     discount += 0.25m;
                 }
                 discountedPrice = GetDiscount(finalPrice, discount);
                 pricesList[i] = discountedPrice;
             }
-            return pricesList;
+            prices = pricesList;
+            return prices;
         }
         private decimal GetDiscount(decimal price, decimal discount)
         {
             decimal result = Math.Round(price - (price * discount), 2);
             return result;
         }
-        private string GetStreet(string adress)
+        private string GetStreet(string address)
         {
-            string[] parts = adress.Split(',');
-            int found = parts[0].IndexOf(" ");
-            string res = parts[0].Substring(found + 1);
+            var parts = address.Split(',');
+            var found = parts[0].IndexOf(" ");
+            var res = parts[0].Substring(found + 1);
             return res;
         }
-        private decimal PriceDiscountAdress(decimal price, string adress) 
+        private decimal PriceDiscountAddress(decimal price, string address) 
         {
-            if (adress.Contains("Wayne Street")){ price = price + 10; }
-            if (adress.Contains("North Heather Street")) { price = price - 5.36m; }
+            if (address.Contains("Wayne Street")){ price = price + 10; }
+            if (address.Contains("North Heather Street")) { price = price - 5.36m; }
             return price;
         }
         public decimal СurrencyConverter(decimal euroPrice)
         {
-           decimal dollarPrice = euroPrice * 1.19m;
-           decimal result = Math.Round(dollarPrice, 2);
+           var dollarPrice = euroPrice * 1.19m;
+           var result = Math.Round(dollarPrice, 2);
            return result;
         }
         public decimal InvokePriceCalculatiion()
